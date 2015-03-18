93c43edf08157570ea67b48cd3fb53bac373f6f9
Bosco So <boscoso@pif-bosco-mac.local>
2015-03-17 18:59:24 -0700

    displaying tags in a Math.log weighted manner

--- tagcloud/tagcloud.js
+++ tagcloud/tagcloud.js
@@ -5,11 +5,26 @@ jQuery("document").ready(function (event) {
     var container = jQuery('#tagList');
 
     var ajax = jQuery.ajax('keyword_counts.json', {dataType: "json"});
+
+    // configuration
+    var gradient = {
+        0:    '#f00', // red
+        0.33: '#ff0', // yellow
+        0.66: '#0f0', // green
+        1:    '#00f'  // blue
+    };
+    TagCanvas.weight = true;
+    TagCanvas.weightGradient = gradient;
+    TagCanvas.weightFrom = 'data-weight';
+    TagCanvas.weightMode = 'both';
+
+    // start the whole thing off with Ajax call
     ajax.done(function(rawdata) {
         var arry = rawdata.data;
         var filtered = arry.reduce(
             function(buffer, item, ii) {
-                if (item.frequency > 10) {
+                if (item.frequency > 20) {
+                    item.frequency = Math.log(item.frequency) * 10;
                     buffer.push(item);
                 }
                 return buffer;
@@ -26,10 +41,17 @@ jQuery("document").ready(function (event) {
 
         // fill tag canvas
         try {
-            TagCanvas.Start('myCanvas', 'tagList');
+            TagCanvas.Start('myCanvas',
+                            'tagList',
+                            {
+                                // textColour: 'black',
+                                maxSpeed: 0.05
+                            }
+                           );
         } catch(e) {
             // something went wrong, hide the canvas container
-            document.getElementById('myCanvasContainer').style.display = 'none';
+            // document.getElementById('myCanvasContainer').style.display = 'none';
+            console.log('ERROR', e);
         }
     });
 
